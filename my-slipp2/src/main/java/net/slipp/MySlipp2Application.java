b7792d712bc332bad167f08c493d35d40ce01099
wotjd4305 <wotjd4305@naver.com>
2019-09-18 00:57:07 +0900

    9/18
    1)로컬로 연결하여 브라우저에 띄우는 작업(LiveReload 사용)
    2-1)j쿼리, 부트스트랩 라이브러리 사용하기
    2-2)부트스트랩의 html 디자인 가져다쓰기

---
+++ my-slipp2/src/main/java/net/slipp/MySlipp2Application.java
@@ -0,0 +1,13 @@
+package net.slipp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySlipp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySlipp2Application.class, args);
+	}
+
+}
