9979f6680458bdc8a493e12a696c9bb9708b4b8c
wotjd4305 <wotjd4305@naver.com>
2019-09-20 18:33:10 +0900

    9/20 2-1. 컨트롤러 이용, 머시테시 써보기
    오류나서 프로젝트 다시만듬...

---
+++ my-slipp3-1/src/main/java/net/slipp3/MySlipp31Application.java
@@ -0,0 +1,13 @@
+package net.slipp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySlipp31Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySlipp31Application.class, args);
+	}
+
+}
---
+++ my-slipp3/src/main/java/net/slipp3/web/WelcomeController.java
@@ -0,0 +1,11 @@
+package net.slipp3.web;
+
+import 
+
+@Controller
+public class WelcomeController {
+	
+	public String welcome() {
+		return "welcome";//temlates밑에 있는 파일을 호출. 즉 welcome파일 호출
+	}
+}
