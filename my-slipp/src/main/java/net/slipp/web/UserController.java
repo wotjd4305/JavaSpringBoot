e6d6931a053ff2aaae97b21d931c69c35d2d54f0
wotjd4305 <wotjd4305@naver.com>
2019-09-26 22:09:40 +0900

    9/26 서버연동문제

--- my-slipp/src/main/java/net/slipp/web/UserController.java
+++ my-slipp/src/main/java/net/slipp/web/UserController.java
@@ -3,12 +3,16 @@ package net.slipp.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@EnableAutoConfiguration
+@ComponentScan(basePackages= {"net.slipp.web"})
 public class UserController {
 	private List<User> users = new ArrayList<User>();
 	
