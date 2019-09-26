c9f6b605afd4f78e4aefbefaedf807f97f6e8469
wotjd4305 <wotjd4305@naver.com>
2019-09-26 22:27:53 +0900

    9/26
    ㅇㄻ

--- my-slipp/src/main/java/net/slipp/web/UserController.java
+++ my-slipp/src/main/java/net/slipp/web/UserController.java
@@ -7,9 +7,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @EnableAutoConfiguration
@@ -18,7 +17,7 @@ public class UserController {
 	private List<User> users = new ArrayList<User>();
 	
 	//연결해주기
-	@RequestMapping("/")
+	@RequestMapping(value="/create" , method = {RequestMethod.GET, RequestMethod.POST})
 	public String create(User user) {
 		System.out.println("user : " + user);
 		users.add(user);
@@ -26,7 +25,7 @@ public class UserController {
 	}
 	
 	//연결해주기
-	@RequestMapping("/")
+	@RequestMapping(value="/list" , method = {RequestMethod.GET, RequestMethod.POST})
 	public String list(Model model) {
 		model.addAttribute("user", users); 
 		return "list";
